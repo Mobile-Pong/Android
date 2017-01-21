3c5ee068392fb67869ddda3f69823d6a74cc976e
Chandraanshu <Chandraanshu@gmail.com>
2017-01-21 23:49:28 +0800

    accelerometer with gravity corrected

--- MobilePong/app/src/main/java/com/example/chandraanshugarg/mobilepong/MainActivity.java
+++ MobilePong/app/src/main/java/com/example/chandraanshugarg/mobilepong/MainActivity.java
@@ -13,11 +13,14 @@
     import android.widget.TextView;
     import android.widget.Toast;
 
+    import java.text.DecimalFormat;
+
     public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
         private float lastX, lastY, lastZ;
         private SensorManager sensorManager;
         private Sensor accelerometer;
+        private boolean mInitialized = false;
 
         private float deltaX = 0;
         private float deltaY = 0;
@@ -68,19 +71,61 @@
 
         @Override
         public void onSensorChanged(SensorEvent event) {
+
+            double x = event.values[0];
+            double y = event.values[1];
+            double z = event.values[2];
+
+            final double alpha = 0.8;
+
+            double[] gravity = {0, 0, 0};
+
+            gravity[0] = alpha * gravity[0] + (1 - alpha) * event.values[0];
+            gravity[1] = alpha * gravity[1] + (1 - alpha) * event.values[1];
+            gravity[2] = alpha * gravity[2] + (1 - alpha) * event.values[2];
+
+            x = event.values[0] - gravity[0];
+            y = event.values[1] - gravity[1];
+            z = event.values[2] - gravity[2];
+//
+//
+
+
+            //displayMaxValues();
+//            deltaX = Math.abs(lastX - event.values[0]);
+//            deltaY = Math.abs(lastY - event.values[1]);
+//            deltaZ = Math.abs(lastZ - event.values[2]);
+
+            if (!mInitialized) {
+                // sensor is used for the first time, initialize the last read values
+                lastX = (float) x;
+                lastY = (float) y;
+                lastZ = (float) z;
+                mInitialized = true;
+            } else {
+                // sensor is already initialized, and we have previously read values.
+                // take difference of past and current values and decide which
+                // axis acceleration was detected by comparing values
+
+                deltaX = (float) Math.abs(lastX - x);
+                deltaY = (float) Math.abs(lastY - y);
+                deltaZ = (float) Math.abs(lastZ - z);
+                lastX = (float) x;
+                lastY = (float) y;
+                lastZ = (float) z;
+                if (deltaX < 0.1)
+                    deltaX = 0;
+                if (deltaY < 0.1)
+                    deltaY = 0;
+                if (deltaZ < 0.1)
+                    deltaZ = 0;
+//                if ((deltaZ > vibrateThreshold) || (deltaY > vibrateThreshold) || (deltaZ > vibrateThreshold)) {
+//                    v.vibrate(50);
+//                }
+            }
+
             displayCleanValues();
             displayCurrentValues();
-            //displayMaxValues();
-            deltaX = Math.abs(lastX - event.values[0]);
-            deltaY = Math.abs(lastY - event.values[1]);
-            deltaZ = Math.abs(lastZ - event.values[2]);
-            if (deltaX < 2)
-                deltaX = 0;
-            if (deltaY < 2)
-                deltaY = 0;
-            if ((deltaZ > vibrateThreshold) || (deltaY > vibrateThreshold) || (deltaZ > vibrateThreshold)) {
-                v.vibrate(50);
-            }
         }
         public void displayCleanValues() {
             currentX.setText("0.0");
