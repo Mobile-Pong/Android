ebece82c251eed241f8f616ed5a61b3b23006c47
Chandraanshu <Chandraanshu@gmail.com>
2017-01-21 22:16:15 +0800

    accelerometer added

--- MobilePong/app/src/main/java/com/example/chandraanshugarg/mobilepong/MainActivity.java
+++ MobilePong/app/src/main/java/com/example/chandraanshugarg/mobilepong/MainActivity.java
@@ -1,13 +1,100 @@
-package com.example.chandraanshugarg.mobilepong;
+    package com.example.chandraanshugarg.mobilepong;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+    import android.support.v7.app.AppCompatActivity;
+    import android.os.Bundle;
+    import android.app.Activity;
+    import android.content.Context;
+    import android.hardware.Sensor;
+    import android.hardware.SensorEvent;
+    import android.hardware.SensorEventListener;
+    import android.hardware.SensorManager;
+    import android.os.Bundle;
+    import android.os.Vibrator;
+    import android.widget.TextView;
+    import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+    public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        private float lastX, lastY, lastZ;
+        private SensorManager sensorManager;
+        private Sensor accelerometer;
+
+        private float deltaX = 0;
+        private float deltaY = 0;
+        private float deltaZ = 0;
+
+        private float vibrateThreshold = 0;
+
+        private TextView currentX, currentY, currentZ;
+        public Vibrator v;
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            initializeViews();
+            sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+            if (sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
+                accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+                sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+                vibrateThreshold = accelerometer.getMaximumRange() / 2;
+            } else {
+                Toast.makeText(MainActivity.this, "No Accelerometer on Device", Toast.LENGTH_LONG).show();
+            }
+
+            v = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
+        }
+
+        public void initializeViews() {
+            currentX = (TextView) findViewById(R.id.currentX);
+            currentY = (TextView) findViewById(R.id.currentY);
+            currentZ = (TextView) findViewById(R.id.currentZ);
+        }
+
+        protected void onResume() {
+            super.onResume();
+            sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+
+        protected void onPause() {
+            super.onPause();
+            sensorManager.unregisterListener(this);
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            displayCleanValues();
+            displayCurrentValues();
+            //displayMaxValues();
+            deltaX = Math.abs(lastX - event.values[0]);
+            deltaY = Math.abs(lastY - event.values[1]);
+            deltaZ = Math.abs(lastZ - event.values[2]);
+            if (deltaX < 2)
+                deltaX = 0;
+            if (deltaY < 2)
+                deltaY = 0;
+            if ((deltaZ > vibrateThreshold) || (deltaY > vibrateThreshold) || (deltaZ > vibrateThreshold)) {
+                v.vibrate(50);
+            }
+        }
+        public void displayCleanValues() {
+            currentX.setText("0.0");
+            currentY.setText("0.0");
+            currentZ.setText("0.0");
+        }
+
+        public void displayCurrentValues() {
+            currentX.setText(Float.toString(deltaX));
+            currentY.setText(Float.toString(deltaY));
+            currentZ.setText(Float.toString(deltaZ));
+        }
+
+
+
+
     }
-}
